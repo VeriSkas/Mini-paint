22a02497b6d58e5c29f4f33eae1803bd61d80b4b
VeriSkas <92_sveto4ka@mail.ru>
2023-01-03 21:50:42 +0300

    added .env file

--- src/api/apiConfig.tsx
+++ src/api/apiConfig.tsx
@@ -3,14 +3,13 @@ import { getAuth } from 'firebase/auth';
 import { getDatabase } from 'firebase/database';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyDk_pqhC_XlgZYUOJp3KjwsTwNUMrMg_Vw',
-  authDomain: 'mini-paint-924ca.firebaseapp.com',
-  databaseURL:
-    'https://mini-paint-924ca-default-rtdb.europe-west1.firebasedatabase.app/',
-  projectId: 'mini-paint-924ca',
-  storageBucket: 'mini-paint-924ca.appspot.com',
-  messagingSenderId: '416135286735',
-  appId: '1:416135286735:web:4f8b38a8f26077856e8cba',
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  databaseURL: process.env.REACT_APP_databaseURL,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
 };
 
 const app = initializeApp(firebaseConfig);
